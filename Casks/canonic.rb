9e68d1126327b3521f6c8ccbaa492320aded4145
John Azzinaro <johnazzi430@gmail.com>
2026-05-29 22:13:25 -0400

    canonic 0.0.34-alpha
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Casks/canonic.rb
+++ Casks/canonic.rb
@@ -1,8 +1,8 @@
 cask "canonic" do
-  version "0.0.33-alpha"
-  sha256 "a2b4ba1f42ca9b5dc160d9423b52b3a261132e6ce67c725bbf8fafe76ea40819"
+  version "0.0.34-alpha"
+  sha256 "da5b74e54e38424c377dd200ea466606625a3fa302008a92da2818e43780309d"
 
-  url "https://github.com/Canonical-AI/canonic/releases/download/v0.0.33-alpha/canonic-arm64.dmg"
+  url "https://github.com/Canonical-AI/canonic/releases/download/v0.0.34-alpha/canonic-arm64.dmg"
   name "Canonic"
   desc "Local-first markdown editor for product managers"
   homepage "https://github.com/Canonical-AI/canonic"
